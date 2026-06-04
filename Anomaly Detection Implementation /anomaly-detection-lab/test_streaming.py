21091fdf7a22333b98b910263bfa73c1cd26432a
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-03 21:54:44 -0700

    Create test_streaming.py

---
+++ Anomaly Detection Implementation /anomaly-detection-lab/test_streaming.py
@@ -0,0 +1,19 @@
+import pickle
+import numpy as np
+
+def simulate_stream(model_path: str, n_points: int = 20):
+    """
+    Simulate real-time anomaly detection.
+    
+    Args:
+        model_path: Path to trained model
+        n_points: Number of points to simulate
+    """
+    # TODO: Load model
+    # TODO: Generate random data points
+    # TODO: Detect anomalies in real-time
+    # TODO: Print results for each point
+    pass
+
+if __name__ == "__main__":
+    simulate_stream('isolation_forest_model.pkl')
