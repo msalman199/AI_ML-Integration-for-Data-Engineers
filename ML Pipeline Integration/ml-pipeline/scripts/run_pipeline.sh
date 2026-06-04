da7ae72d549b561ee5e4a7135597c9bdfcddd4e9
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-06-03 22:32:33 -0700

    Create run_pipeline.sh

---
+++ ML Pipeline Integration/ml-pipeline/scripts/run_pipeline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ML Pipeline Orchestration Script
+SCRIPT_DIR="$HOME/ml-pipeline/scripts"
+LOG_DIR="$HOME/ml-pipeline/logs"
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+
+# TODO: Activate virtual environment
+
+# TODO: Run data generation script and log output
+
+# TODO: Run training script and log output
+
+# TODO: Check if training was successful and log status
+
+echo "Pipeline completed at $(date)"
